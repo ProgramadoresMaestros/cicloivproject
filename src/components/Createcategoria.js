c95ffa59f1fc617e37884f75b81e1400277ec735
jdpuertas <j.diegopuertas@gmail.com>
2022-11-04 21:32:07 -0500

    primer commit

---
+++ src/components/Createcategoria.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class Createcategoria extends Component{
+    render(){
+        return(
+            <div>Createcategoria</div>
+        )
+    }
+}
---
+++ src/components/Navigation.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+export default class Navigation extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <Link class="navbar-brand" to="/">Menu</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbar">
+                        <ul className="navbar-nav ">
+                            <li className="nav-item">
+                                <Link className="nav-link "  to="/categorias">categorias</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/productos">productos</Link>
+                            </li>
+                            
+                            
+                        </ul>
+                        
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
